1324cbfb3d5b2307b82d8883a09f1b0bbe74e222
Keon Pourboghrat <kekons4@gmail.com>
2024-12-12 09:45:56 -0500

    added a lot of commands to cryptolitics including admin commands and fancy menu

--- cryptolitics-bot.js
+++ cryptolitics-bot.js
@@ -1,9 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 const WebSocket = require('ws');
-require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+// User Defined modules and files
+const env = require("./env.json");
 
 // Add you botfather api token into .env
-const token = process.env.BOTFATHERAPITOKEN;
+const token = env.BOTFATHERAPITOKEN;
+
 // const targetChatId = 'YOUR_CHAT_ID'; // Replace with the actual chat ID
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -54,8 +59,113 @@ const periodicTask = async () => {
     // });
 };
 
-// // Schedule the task to run every 10 seconds (60,000 milliseconds)
-setInterval(periodicTask, 10 * 1000);
+const commandCheck = (msg, chatId) => {
+    switch(msg.text) {
+        case "/kreios":
+            bot.sendMessage(chatId, `This is a secret command... do not share`);
+            break;
+        case "/welcome":
+            bot.sendMessage(chatId, `Welcome to the Big Pharmai ${msg.from.first_name}! Try /menu to start!`);
+            break;
+        case "/ca":
+            bot.sendMessage(chatId, env.CA);
+            break;
+        case "/learn":
+            bot.sendMessage(chatId, env.LEARN);
+            break;
+        case "/menu":
+            const options = {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            { text: 'Available Commands', callback_data: 'nothing' }
+                        ],
+                        [
+                            { text: '👥Twitter/X', url: env.XPROFILE },
+                            { text: '📜CA', callback_data: 'ca' },
+                            { text: '📚Learn', callback_data: 'learn' }
+                        ],
+                        [
+                            { text: '🕸Website', callback_data: 'website' },
+                            { text: '📈Dexscreener', callback_data: "dexscreener" }
+                        ],
+                        [
+                            { text: '❌Close', callback_data: 'closeMenu' }
+                        ]
+                    ]
+                }
+            };
+
+            bot.sendMessage(chatId, "Menu", options);
+            break;
+    }
+}
+
+const adminCommands = (msg, chatId) => {
+    const cmd = msg.text === undefined ? "ERROR" : msg.text.split(" ")[0];
+    const message = msg.text === undefined ? "ERROR" : msg.text.split(" ")[1];
+
+
+    switch(cmd) {
+        case "/setca":
+            env.CA = message;
+            fs.writeFileSync(path.join(process.cwd(), "env.json"), JSON.stringify(env));
+            bot.sendMessage(chatId, `CA has been set to: ${message}`);
+            break;
+        case "/setlearn":
+            env.LEARN = message;
+            fs.writeFileSync(path.join(process.cwd(), "env.json"), JSON.stringify(env));
+            bot.sendMessage(chatId, `CA has been set to: ${message}`);
+            break;
+        case "/btn":
+            // Define inline keyboard buttons
+            const options = {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            { text: '👥Twitter/X', url: env.XPROFILE },
+                            { text: '📜CA', callback_data: 'ca' },
+                            { text: '📚Learn', callback_data: 'learn' }
+                        ],
+                        [
+                            { text: '❌Close', callback_data: 'closeMenu' }
+                        ]
+                    ]
+                }
+            };
+
+            bot.sendMessage(chatId, 'Available Commands', options);
+            break;
+        case "ERROR":
+            console.log("Non-standard message was sent");
+            break;
+    }
+}
+
+// Handle callback queries
+bot.on('callback_query', (callbackQuery) => {
+    const message = callbackQuery.message;
+    const data = callbackQuery.data;
+
+    if (data === 'ca') {
+        bot.sendMessage(message.chat.id, env.CA);
+    } else if (data === 'learn') {
+        bot.sendMessage(message.chat.id, env.LEARN);
+    } else if (data === 'closeMenu') {
+         // Delete the previous message
+         bot.deleteMessage(message.chat.id, message.message_id)
+            .then(() => {
+                console.log(`Bot deleted message successfully | chatId: ${message.chat.id} | messageId: ${message.message_id}`);
+            })
+            .catch((err) => {
+                console.error(`Failed to delete message | chatId: ${message.chat.id} | messageId: ${message.message_id} | ERROR: `, err);
+            });
+    } else if(data === "nothing") {
+        console.log("Do nothing...");
+    } else {
+        bot.sendMessage(message.chat.id, "Coming soon!");
+    }
+});
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -68,27 +178,44 @@ bot.on('message', async (msg) => {
             const memberInfo = await bot.getChatMember(chatId, userId);
             const userRole = memberInfo.status; // Possible values: "creator", "administrator", "member", "restricted", "left", "kicked"
 
-            if (userRole === 'creator' || userRole === 'administrator') {
-                bot.sendMessage(chatId, `Hello Admin, ${msg.from.first_name}!`);
+            if (userRole === 'creator' || userRole === 'administrator' || userRole === 'Sythographer') {
+                // bot.sendMessage(chatId, `Hello Admin, ${msg.from.first_name}!`);
+                commandCheck(msg, chatId);
+                adminCommands(msg, chatId);
+            } else if (userRole === 'Bot'){
+                commandCheck(msg, chatId);
             } else if (userRole === 'member') {
-                bot.sendMessage(chatId, `Hi ${msg.from.first_name}, you’re a member of this group.`);
+                // bot.sendMessage(chatId, `Hi ${msg.from.first_name}, you’re a member of this group.`);
+                commandCheck(msg, chatId);
             } else {
-                bot.sendMessage(chatId, `Sorry ${msg.from.first_name}, you don’t have permission to use this command.`);
+                // bot.sendMessage(chatId, `Sorry ${msg.from.first_name}, you don’t have permission to use this command.`);
+                commandCheck(msg, chatId);
             }
         } catch (error) {
             console.error('Error fetching chat member info:', error);
             bot.sendMessage(chatId, 'Unable to determine your permissions.');
         }
     }
-
-    // switch(msg.text) {
-    //     case "/kreios":
-    //         bot.sendMessage(chatId, `This is a secret command... do not share`);
-    //         break;
-    //     case "/welcome":
-    //         bot.sendMessage(chatId, `Welcome to the Cryptolitics ${msg.from.first_name}!`)
-    //         break;
-    // }
 });
 
+// Listen for new chat members
+bot.on('new_chat_members', (msg) => {
+    // Get the group chat ID
+    const chatId = msg.chat.id;
 
+    // Loop through all new members (in case multiple users join simultaneously)
+    msg.new_chat_members.forEach((newMember) => {
+        // Get the new member's first name
+        const firstName = newMember.first_name || 'NULL';
+        const lastName = newMember.last_name || '';
+
+        // Customize your welcome message
+        const welcomeMessage = `Welcome ${firstName} ${lastName} to Big Pharmai! 🎉 Try /menu to start!`;
+
+        // Send the welcome message
+        bot.sendMessage(chatId, welcomeMessage);
+    });
+});
+
+// // Schedule the task to run every 10 seconds (60,000 milliseconds)
+// setInterval(periodicTask, 10 * 1000);
